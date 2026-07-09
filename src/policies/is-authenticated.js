73cde64d7bfbdc8d4302d5d25575681f4c9aa424
Hoodlaces <steve@hoodlaces.com>
2026-07-08 23:32:15 -0700

    Diagnostic: log is-authenticated policy factory/invocation to root-cause policy resolution failure

--- src/policies/is-authenticated.js
+++ src/policies/is-authenticated.js
@@ -5,7 +5,9 @@
  */
 
 module.exports = (policyContext, config, { strapi }) => {
+  strapi.log.info('[DIAGNOSTIC] is-authenticated policy FACTORY called');
   return async (ctx, next) => {
+    strapi.log.info(`[DIAGNOSTIC] is-authenticated policy INVOKED, has user: ${!!ctx.state.user}`);
     if (ctx.state.user) {
       // User is authenticated
       return await next();
